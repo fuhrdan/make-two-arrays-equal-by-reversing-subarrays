935fbd339576a6618fb96fdb65c6b3a72989a501
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-08-03 17:29:41 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//*****************************************************************************
+//** 1640. Make Two Arrays Equal by Reversing Subarrays  leetcode            **
+//** I used a hash table instead of reversing the arrays.  I feel like this  **
+//** I feel this wasn't in the spirit of the problem, but a working solution **
+//** is a working solution.                                           -Dan   **
+//*****************************************************************************
+
+
+bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
+    int hash[1002];
+    int retnum = -1;
+    for(int i = 0; i < arrSize; i++)
+    {
+        hash[arr[i]]++;
+    }
+    for(int i = 0; i < targetSize; i++)
+    {
+        hash[target[i]]--;
+        if(hash[target[i]] < 0) return 0;
+    }
+    return 1;
+
+}
